9ac81e6313773ab78afa66fa1ed789a695cba0ef
Hong-Phuc Bui <hbui@mint>
2013-07-03 13:15:42 +0200

    test

--- makeline.lua
+++ makeline.lua
@@ -1,11 +1,9 @@
 doc_prefix=[[
 \documentclass[11pt,a4paper]{article}
-%\usepackage{luacode,luaotfload}
 \usepackage{fontspec}
 \usepackage{geometry}
 \usepackage{tikz}
 \usetikzlibrary{arrows,calc,positioning,shapes.geometric}
-%\usepackage{a4wide}
 \usepackage{layout}
 \setcounter{secnumdepth}{-1}
 \geometry{
@@ -13,7 +11,7 @@ doc_prefix=[[
 }
 \usepackage[hidelinks]{hyperref}
 
-\title{Font-List}
+\title{%s}
 \author{Hong Phuc Bui}
 \date{27.\,06.\,2013}
 
@@ -40,12 +38,13 @@ tikz_prefix = [[
   noname/.style={%
     rectangle, %rounded corners=9pt,
     text height=1em,
-    text depth=-1ex,
-    text width=1ex,
+    text depth=2ex,
+    text width=1.25ex,
     text centered,
-    minimum height=2.5cm,
-    minimum width=2cm,
-    inner sep=1.ex,
+    minimum height=.5cm,
+    minimum width=1.5cm,
+    inner xsep=0.ex,
+	inner ysep=1ex,
     %draw=black!50,fill=black!20
     draw=black
   }
@@ -58,6 +57,20 @@ tikz_suffix = [[
 
 text_probe = 
 [[Vögel üben Gezwitscher oft ähnlich packend wie Jupp die Maus auf dem Xylophon einer Qualle. 1234567890]]
+
+
+function contains(t, e)
+	for i = 1,#t do
+		if t[i] == e then return true end
+	end
+	return false
+end
+
+
+function str_sort(a, b)
+	return string.lower(a) < string.lower(b)
+end
+
 
 function make_probe_text()
 	local cmd = [[
@@ -76,26 +89,26 @@ function makeline(vector, line_length, f)
 	local latex_code = tikz_prefix .. "\n"
 	f(tikz_prefix)
 
-	local first_line = [[\node[noname] (1)              {\symbol{%04d}\\ \textnormal{\symbol{%04d}}};]]
+	local first_line = [[\node[noname] (1)              {\symbol{%04d}\\ \textnormal{\symbol{%04d} \texttt{%02d}}};]]
 
-	local line = string.format(first_line,vector[1], vector[1])
+	local line = string.format(first_line, vector[1], vector[1], vector[1], vector[1])
 	
 	f(line)
 	latex_code = latex_code .. line .. "\n"
 
-	local midle_line = [[\node[noname] (%i) [right=of %i] {\symbol{%04d}\\ \textnormal{\symbol{%04d}}};]]
+	local midle_line = [[\node[noname] (%i) [right=of %i] {\symbol{%04d}\\ \textnormal{\symbol{%04d} \texttt{%02d}}};]]
 	local base_line =  [[\draw[help lines,red, -] let \p1 = (%i.base), \p2 = (%i.base) in (-0.5ex,\y1) -- (\x2+1ex,\y1) ;]]
-	local below_line = [[\node[noname] (%i) [below=of %i] {\symbol{%04d}\\ \textnormal{\symbol{%04d}}};]]
+	local below_line = [[\node[noname] (%i) [below=of %i] {\symbol{%04d}\\ \textnormal{\symbol{%04d} \texttt{%02d}}};]]
 	local mark = 1
 	for i = 2,#vector do
 		local c = vector[i]
 		if i % line_length == 1 then
-			line = string.format(below_line,i, i-line_length,c,c)
+			line = string.format(below_line,i, i-line_length,c,c,c)
 			f(line)
 			latex_code = latex_code .. line .. "\n"
 			mark = i
 		else
-			line = string.format(midle_line,i,i-1,c,c)
+			line = string.format(midle_line,i,i-1,c,c,c)
 			f(line)
 			latex_code = latex_code .. line .. "\n"
 		end
@@ -117,12 +130,61 @@ function makeline(vector, line_length, f)
 	return latex_code
 end
 
-function maketable(from, to, step, tab)
-	tab= {}
+function make_table(from, to, step, tab)
 	for i = from, to, step do
-		print i
 		tab[#tab+1] = i
 	end
+end
+
+function write_glyphen_test_file(font_family, font_name, tex_file)
+	tex_file:write(string.format(doc_prefix,font_family))
+	
+	
+	for _,fn in ipairs(font_name) do
+		tex_file:write(string.format([[\section{\fontspec[Ligatures=TeX,Scale=1]{%s}%s}]].."\n",fn,fn))
+		local set_font_code = string.format([[\fontspec[Ligatures=TeX,Scale=2]{%s}]] .. "\n", fn)
+		tex_file:write(set_font_code)
+		
+		local latex_code = make_block(0x0021,0x007E,"basic latin")
+		tex_file:write(latex_code)
+		
+		local latex_code = make_block(0x00A1, 0x00FF,"latin supplement")
+		tex_file:write(latex_code)
+
+		--local latex_code = make_block(0x0100, 0x017F,"latin extended A")
+		--tex_file:write(latex_code)
+		
+		--local latex_code = make_block(0x0400, 0x04FF,"Cyrillic")
+		--tex_file:write(latex_code)
+		
+		--local latex_code = make_block(0x0500, 0x052F,"Cyrillic Supplement")
+		--tex_file:write(latex_code)
+		
+	end
+	tex_file:write(doc_suffix)
+end
+
+function make_block(begin_letter, end_letter, name,font_name)
+	local block_length = 5
+	local line_length = 11
+	local begin_block = begin_letter
+	local last = begin_block + block_length*line_length -1
+	local end_block = (last < end_letter) and last or end_letter
+	local latex_code = string.format("\\subsection{%s}\n",name)
+
+	-- print ("end_letter", end_letter)
+	while begin_block < end_letter do
+		-- print ("begin_block", begin_block,"end_block",end_block)
+		local b = {}
+		make_table(begin_block, end_block, 1, b)
+		latex_code = latex_code .. makeline(b,line_length,function(a) end)
+
+		begin_block = end_block + 1
+		last = begin_block + block_length*line_length -1
+		-- print ("next last", last)
+		end_block = (last < end_letter) and last or end_letter
+	end
+	return latex_code
 end
 
 
